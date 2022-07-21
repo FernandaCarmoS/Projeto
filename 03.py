b79cc9f2771b6663b26c68c798918e8688eb4ded
fernanda <fernandadocarmo1@outlook.com>
2022-07-21 19:43:44 -0300

    03

---
+++ 03.py
@@ -0,0 +1,6 @@
+print('CRIE UM PROGRAMA QUE LEIA DOIS NÙMEROS E MOSTRE A SOMA ENTRE ELES.')
+print('Resolução:')
+n1 = int(input('Digite um número - '))
+n2 = int(input('Digite outro número - '))
+soma = n1 + n2
+print('A soma entre {} e {} é {}! '.format(n1, n2, soma))
