36e95cbec8a80818a9ed55cfa8a82071657f1a44
fernanda <fernandadocarmo1@outlook.com>
2022-07-21 19:42:28 -0300

    02

---
+++ 02.py
@@ -0,0 +1,5 @@
+
+print('CRIE UM PROGRAMA NO QUAL A PESSOA DIGITE O NOME DELA E NA TELA APAREÇA A MENSAGEM “ É UM PRAZER TE CONHECER, NOME DA PESSOA!')
+print('Resolução: ')
+nome = input('Olá, qual é o seu nome? - ')
+print('È um prazer te conhecer {}! '.format(nome))
