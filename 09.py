45d4e57efde30a585da9bbf49c85194c773b48fc
fernanda <fernandadocarmo1@outlook.com>
2022-07-24 20:08:41 -0300

    09

---
+++ 09.py
@@ -0,0 +1,95 @@
+print('FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO QUALQUER E MOSTRE NA TELA A SUA TABUADA.')
+num = int(input('Digite um número para que possamos te dar a tabuada completa! '))
+num1 = num * 1
+num2 = num * 2
+num3 = num * 3
+num4 = num * 4
+num5 = num * 5
+num6 = num * 6
+num7 = num * 7
+num8 = num * 8
+num9 = num * 9
+num10 = num * 10
+print('A tabuada de multiplicação de {} é:\n'
+      '{} vezes 1 é igual a {}\n'
+      '{} vezes 2 é igual a {}\n'
+      '{} vezes 3 é igual a {}\n'
+      '{} vezes 4 é igual a {}\n'
+      '{} vezes 5 é igual a {}\n'
+      '{} vezes 6 é igual a {}\n'
+      '{} vezes 7 é igual a {}\n'
+      '{} vezes 8 é igual a {}\n'
+      '{} vezes 9 é igual a {}\n'
+      '{} vezes 10 é igual a {}\n'
+      ''.format(num, num, num1, num, num2, num, num3, num, num4, num, num5,num, num6, num, num7, num, num8, num, num9,num, num10))
+div1 = num / 1
+div2 = num / 2
+div3 = num / 3
+div4 = num / 4
+div5 = num / 5
+div6 = num / 6
+div7 = num / 7
+div8 = num / 8
+div9 = num / 9
+div10 = num / 10
+print('A tabuada de divisão de {} é:\n'
+      '{} dividido por 1 é igual a {:.2f}\n'
+      '{} dividido por 2 é igual a {:.2f}\n'
+      '{} dividido por 3 é igual a {:.2f}\n'
+      '{} dividido por 4 é igual a {:.2f}\n'
+      '{} dividido por 5 é igual a {:.2f}\n'
+      '{} dividido por 6 é igual a {:.2f}\n'
+      '{} dividido por 7 é igual a {:.2f}\n'
+      '{} dividido por 8 é igual a {:.2f}\n'
+      '{} dividido por 9 é igual a {:.2f}\n'
+      '{} dividido por 10 é igual a {:.2f}\n'
+      .format(num,num, div1,num, div2, num, div3, num, div4, num, div5, num, div6, num, div7, num, div8, num , div9, num, div10))
+soma1 = num + 1
+soma2 = num + 2
+soma3 = num + 3
+soma4 = num + 4
+soma5 = num + 5
+soma6 = num + 6
+soma7 = num + 7
+soma8 = num + 8
+soma9 = num + 9
+soma10 = num + 10
+print('A Soma de {} é:\n'
+      '{} + 1 é igual a {} \n'
+      '{} + 2 é igual a {} \n'
+      '{} + 3 é igual a {} \n'
+      '{} + 4 é igual a {} \n'
+      '{} + 5 é igual a {} \n'
+      '{} + 6 é igual a {} \n'
+      '{} + 7 é igual a {} \n'
+      '{} + 8 é igual a {} \n'
+      '{} + 9 é igual a {} \n'
+      '{} + 10 é igual a {} \n'
+      .format(num, num, soma1,num, soma2,num,soma3,num, soma4,num, soma5, num, soma6, num, soma7, num, soma8, num, soma9, num, soma10))
+sub1 = num - 1
+sub2= num - 2
+sub3 = num - 3
+sub4 = num - 4
+sub5 = num - 5
+sub6 = num - 6
+sub7 = num - 7
+sub8 = num - 8
+sub9 = num - 9
+sub10 = num - 10
+print('A Subtração de {} é:\n'
+      '{} menos 1 é igual a {}\n'
+      '{} menos 2 é igual a {}\n'
+      '{} menos 3 é igual a {}\n'
+      '{} menos 4 é igual a {}\n'
+      '{} menos 5 é igual a {}\n'
+      '{} menos 6 é igual a {}\n'
+      '{} menos 7 é igual a {}\n'
+      '{} menos 8 é igual a {}\n'
+      '{} menos 9 é igual a {}\n'
+      '{} menos 10 é igual a {}\n'
+      .format(num, num, sub1, num,sub2, num,sub3, num, sub4, num,sub5, num,sub6, num, sub7, num,sub8,num ,sub9,num ,sub10))
+
+
+
+
+
