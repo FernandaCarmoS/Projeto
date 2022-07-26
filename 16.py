436ebd1212e5964f2bdfeb6230acc925102fdd36
FernandaCarmoS <106889409+FernandaCarmoS@users.noreply.github.com>
2022-07-26 19:08:50 -0300

    Update 16.py

--- 16.py
+++ 16.py
@@ -5,3 +5,7 @@ print('CRIE UM PROGRAMA QUE LEIA UM NÚMERO REAL QUALQUER PELO TECLADO E MOSTRE 
 num = float(input('Digite um número real(Com pontos flutuantes)! - '))
 print('O número {} tem a porta inteira de {} !'.format(num,trunc(num)))
 # Como importamos através do código from o trunc, nós não precisamos usar o math novamente no format.
+# E podemos também criar esse código sem importar nenhuma biblioteca math como demonstrado abaixo.
+# num = float(input('Digite um número real(Com pontos flutuantes)! - '))
+# print('O número {} tem a porta inteira de {} !'.format(num, int(num)))
+# O código int(inteiro) deixará o número inteiro.
