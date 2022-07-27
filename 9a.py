7a54cacc2966f5e23133d84fa2d397715125d9bc
FernandaCarmoS <106889409+FernandaCarmoS@users.noreply.github.com>
2022-07-27 19:39:19 -0300

    Delete 9a.py

--- 9a.py
+++
@@ -1,4 +0,0 @@
-import emoji
-print("Vamos descobrir se você é a minha alma gêmea? ")
-a = input(str('Me diga o seu nome que eu te responderei! '))
-print(emoji.emojize('Eu te amo {} :sparkling_heart:'.format(a), language='alias'))
