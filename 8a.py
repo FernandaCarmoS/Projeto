5014319416a8a407af7f14585685266c187282de
FernandaCarmoS <106889409+FernandaCarmoS@users.noreply.github.com>
2022-07-27 19:37:00 -0300

    Delete 8a.py

--- 8a.py
+++
@@ -1,8 +0,0 @@
-print('Programa para descobrir se o número é par ou impar!')
-a = int(input('Digite um número qualquer! '))
-resto = a % 2
-if resto == 0:
-    print('O número {} é par'.format(a))
-elif resto != 0:
-    print('O número {} é impar'.format(a))
-    print('Fim do programa')
